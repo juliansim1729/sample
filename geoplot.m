82d53024dc82337475137515df79ac735bbfdc5f
Julian Sim <54722630+juliansim1729@users.noreply.github.com>
2021-05-23 22:33:32 -0400

    Create geoplot.m

---
+++ geoplot.m
@@ -0,0 +1,6 @@
+lat_szechuan = 40.79279959613859
+lon_szechuan = -77.86327921094302
+
+geoplot(lat_szechuan, lon_szechuan, '-*')
+geolimits([40.7923 40.7933], [-77.8638 -77.8628])
+geobasemap satellite
